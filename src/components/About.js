0d0aee5a4ec4934c12f32c026035a25512c0c264
rushi444 <rushi44@gmail.com>
2020-06-27 19:43:14 -0700

    added about us

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,21 @@ import styled from "@emotion/styled"
 import aboutImage from "../images/insert-picture-here.jpg"
 export const About = () => {
   return (
-    <AboutContainer id='about'>
+    <AboutContainer id="about">
       <h1>About Us</h1>
       <AboutUsDetails>
         <img src={aboutImage} alt="insert image" />
         <p>
-          Mad Bun is..............a paragraph about the company...............
-          Let me know about background color here
+          Madbun specializes in sliders, burgers, loaded fries, and chicken
+          tenders with a taste of Asian street food culture in every bite. We
+          take Asian flavors and mix it with a California flare to give our
+          guests a brand new twist on Asian American comfort food. The founders
+          of Madbun come from a Korean, Filipino, Vietnamese, and American
+          upbringing so you’ll be sure to find bits and pieces of each culture
+          in our various items. Our number 1 goal is to serve delicious Asian
+          American comfort food. Every slider and burger is carefully
+          handcrafted using high quality ingredients and house made sauces. With
+          each bite, we strive to make sure our guests go mad for Madbun!
         </p>
       </AboutUsDetails>
     </AboutContainer>
